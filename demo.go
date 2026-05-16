68a1f1ab55186daf2d2c58503d92f16652133369
Timothy Olisaeloka Ngonadi <timothyolisaeloka@gmail.com>
2026-05-16 19:41:21 +0800

    first commit

---
+++ demo.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("Hello World")
+	//var name = "Timothy"
+	//fmt.Println(name)
+	//var age uint8 = 255
+	//fmt.Println(age)
+	//myClass := "level 200" // implicit defined type
+	//fmt.Println(myClass)
+
+	str := "-"
+
+	for idx := 0; idx < len(str); idx++ {
+		fmt.Printf("%c \n \n", str[idx])
+	}
+
+	fullName := "Timothy Olisaeloka Ngonadi"
+	// loop through a string
+	for _, char := range fullName {
+		fmt.Printf("%c", char)
+	}
+
+	var array [2]bool
+	array[0] = true
+	array[1] = true
+	fmt.Println(array)
+	fmt.Println(len(array))
+
+	myArray := [...][2]int{{1, 2}, {3, 4}, {5, 6}}
+	// This passes a copy of the original array to the function,
+	// it does not mutate the underlying array
+	test(myArray)
+	fmt.Println(myArray)
+}
+
+func test(array [3][2]int) {
+	array[0] = [2]int{100, 200}
+}
